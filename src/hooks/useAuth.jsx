142034fb33568cbb7ddf65627f9003dcad5142de
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-06-25 23:32:42 +0600

    add useauth

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,10 +1,1 @@
-import { useContext } from 'react';
-import { AuthContext } from '../providers/AuthProvider';
 
-const useAuth = () => {
-  const authUtils = useContext(AuthContext);
-
-  return authUtils;
-};
-
-export default useAuth;
